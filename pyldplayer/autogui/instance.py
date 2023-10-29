048ddf14102643d7b7c7d4aa95ff375b1e69207d
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-10-29 11:28:06 -0700

    struct: autogui/instance

---
+++ pyldplayer/autogui/instance.py
@@ -0,0 +1,35 @@
+
+
+class LDAutoInstance:
+    def volumeUp(self):
+        pass
+
+    def volumeDown(self):
+        pass
+
+    def fullscreen(self):
+        pass
+
+    def operationRecorder(self):
+        pass
+
+    def installApkDialog(self):
+        pass
+
+    def nativeScreenshot(self):
+        pass
+
+    def screenshot(self):
+        pass
+
+    def shake(self):
+        pass
+
+    def sharedFolderPC(self):
+        pass
+
+    def sharedFolderAndroid(self):
+        pass
+
+    def virtualGpsDialog(self):
+        pass
