aa008eb79a22d6d89bb506d865fe41005c98a6c5
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-10-29 10:39:56 -0700

    env: misc update

--- pyldplayer/__init__.py
+++ pyldplayer/__init__.py
@@ -3,6 +3,7 @@
 from pyldplayer.core.instance import LDConsoleInstance
 from pyldplayer.core.player import LDConsolePlayer
 from pyldplayer.core.config import LDPlayerConfig
+from pyldplayer.core.app import LDPlayerApp
 
 # mapping
 import pyldplayer.mapping as mapping
