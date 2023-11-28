396db2952448f0eeabe3ea81006d62c7181e6afb
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-11-27 19:02:34 -0800

    feat: add init

---
+++ pyldplayer/__init__.py
@@ -0,0 +1,3 @@
+from pyldplayer.console.config import LDModifyConfigDict
+from pyldplayer.console.ldconsole import LDConsole
+from pyldplayer.console.player import LDConsolePlayer, LDConsoleInstance
