67c696f2dd6c58d00ff3e7b96ffed52f4ceaad10
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2023-11-27 19:45:03 -0800

    env: update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+import os
+# walk file in pyldplayer
+folders = []
+for root, dirs, files in os.walk("pyldplayer"):
+    for dir in dirs:
+        if dir == "__pycache__":
+            continue
+        folders.append(os.path.join(root, dir))
+
 
 setup(
     name="pyldplayer",
-    version="1.0.0a0",
+    version="1.0.0a1",
     author="Zackary W",
     description="python wrapper for ldplayer",
-    packages=find_packages("pyldplayer"),
+    packages=folders,
     install_requires=[
         "psutil",
         "pydantic",
